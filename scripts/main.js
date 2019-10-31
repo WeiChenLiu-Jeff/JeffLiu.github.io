e9d516a30b9fee54a807568f541c4ceef3476eb0
Jeff Liu <liu.jeff.tw@gmail.com>
2019-10-31 19:09:29 +0800

    CREATE: SCSS Workflow, Menu option

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,9 @@ function expendMenu(e) {
     e.classList.toggle("active");
     document.getElementById("menu").classList.toggle("active");
 }
+
+function subgo() {
+    // TODO: input option value and change main content.
+    console.log('Option Clicked.');
+    document.getElementById("menu").classList.toggle("active");
+}
