459f1fbc590a2d7a2fffb2a7bbed1d34d58bde90
Jeff Liu <liu.jeff.tw@gmail.com>
2019-11-25 12:22:44 +0800

    Finish: menu select -> topic and panel switch.

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,46 @@
+var currentPanel;
+
 function expendMenu(e) {
     // toggle menu open/close
-    document.getElementById("nav").classList.toggle("active");
+    document.getElementById('nav').classList.toggle('active');
 }
 
 function changeTopic(option) {
+
     // close menu
-    document.getElementById("nav").classList.toggle("active");
+    document.getElementById('nav').classList.toggle('active');
 
-    var topic = document.getElementById("topic");
+    if (option != currentPanel) {
+        var topic = document.getElementById('topic');
 
-    topic.style.opacity = 0;
-    setTimeout(function () {
-        topic.innerHTML = menu[option].title;
-        topic.style.opacity = 1;
-    }, 500)
-    switchPanel(option);
+        topic.style.opacity = 0;
+
+        setTimeout(function () {
+            topic.innerHTML = menu[option].title;
+            topic.style.opacity = 1;
+        }, 500)
+        switchPanel(option);
+    }
+
 }
 
 function switchPanel(option) {
-    
+
+    var panels = document.getElementById('panels');
+    var allPanel = document.getElementsByClassName('panel');
+
+    panels.style.opacity = 0;
+
+    setTimeout(function () {
+        // remove 'active' class from all panel.
+        for (i = 0; i < allPanel.length; i++) {
+            allPanel[i].classList.remove('active');
+        }
+        // add 'active' class to select panel
+        document.getElementById(menu[option].panel).classList.add('active');
+        panels.style.opacity = 1;
+    }, 500)
+
+    currentPanel = option;
+
 }
